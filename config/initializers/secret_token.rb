716f5550220c55528dc51d7cf51d3907da97c56c
Martin J. Kurz <martin.j.kurz@googlemail.com>
2011-09-09 13:57:44 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invaders::Application.config.secret_token = '4e557ef11f26d86c0b9771c6894fb16d3eb36c5fd0c365c27da1577ee3b30d4eb775d7ffce8569dd03721dc37194bddedd9868ad2745e1b11c16f3dc82bfb3e5'
