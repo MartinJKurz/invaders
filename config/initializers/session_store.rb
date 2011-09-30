a9bb6f44f1322bb93f054f752b0efded73a12e9a
Martin J. Kurz <martin.j.kurz@googlemail.com>
2011-10-01 00:01:50 +0200

    fixed mess ?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Invaders::Application.config.session_store :cookie_store, key: '_invaders_session'
+# Invaders::Application.config.session_store :cookie_store, :key '_invaders_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
